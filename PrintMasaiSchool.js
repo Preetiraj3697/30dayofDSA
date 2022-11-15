bcb597aab6533988cabebbf73791ab167cf25050
Preetiraj3697 <satendra11kum@gmail.com>
2022-11-15 21:51:08 +0530

    add

---
+++ PrintMasaiSchool.js
@@ -0,0 +1,24 @@
+// Print Masai School 
+// Description
+
+// You need to print "Masai School" on a line, and print "A Transformation in Education" on a new line.
+
+
+// Input
+// No input
+
+
+// Output
+// Print "Masai School" on a line, and Print "A Transformation in Education" on a new line.
+
+
+// Sample Input 1 
+// No Input
+
+// Sample Output 1
+// Masai School
+// A Transformation in Education
+
+//Answer
+
+console.log("Masai School"+"\n"+"A Transformation in Education")
