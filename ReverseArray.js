78d0956cba6d51101eeefea80b6c568cf2cb8ba1
Preetiraj3697 <satendra11kum@gmail.com>
2022-11-16 10:17:05 +0530

    add

---
+++ ReverseArray.js
@@ -0,0 +1,16 @@
+//Reverse Array
+// Input  : arr[] = {1, 2, 3}
+// Output : arr[] = {3, 2, 1}
+
+// Input :  arr[] = {4, 5, 1, 2}
+// Output : arr[] = {2, 1, 5, 4}
+
+function fun(num){
+  
+   for(let i=num; i>0; i--){
+      let bag=i+" "
+      console.log(bag)
+   }
+  
+}
+fun(3)
