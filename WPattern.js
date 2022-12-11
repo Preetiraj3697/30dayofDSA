a4f4ab0257f05785224f8260d6b12d2a804a97f6
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2022-12-11 16:12:55 +0530

    Create WPattern.js
    Co-Authored-By: RockAmit <115550644+RockAmit@users.noreply.github.com>

---
+++ WPattern.js
@@ -0,0 +1,78 @@
+// Pattern of W Ended
+// Description
+
+// You need to print pattern as given in Sample Input.
+// Note:- Make sure to check for spaces. In output, there is no spaces at end of each line.
+
+
+
+// Input
+// First line contains N. (1<= N <= 10)
+
+
+// Output
+// Print pattern for N.
+
+
+// Sample Input 1 
+
+// 1
+// Sample Output 1
+
+// \/\/
+// Sample Input 2 
+
+// 4
+// Sample Output 2
+
+// \      /\      /
+//  \    /  \    /
+//   \  /    \  /
+//    \/      \/
+
+function fun(n){
+        
+    for(let i=0; i<n; i++){
+         let bag= "";
+         for(let j=0; j<2*n; j++){
+             if(i==j){
+                 bag+="\\";
+             }else if(i+j==2*n-1){
+                 bag+="/";
+             }else{
+                 bag+=" ";
+             }
+         }
+         console.log(bag+bag);
+    }
+    
+}
+
+function runProgram(input){
+//Input Taking Here
+input = input.trim().split("\n");
+let n = +input[0];
+fun(n);
+
+
+}
+if(process.env.USER===""){
+runProgram('');
+}else{
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let read="";
+process.stdin.on("data",function(input){
+    read+=input;
+});
+process.stdin.on("end",function(){
+    read=read.replace(/\n$/,"");
+    read=read.replace(/\n$/,'');
+    runProgram(read);
+});
+process.on('SIGINT',function(){
+    read=read.replace(/\n$/,"");
+    runProgramm(read);
+    process.et(0);
+});
+}
