8fe71dc18e5bd7509d89f0fdc9cc22df3ad353c1
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2022-12-11 18:04:37 +0530

    Create RemoveMaximumElement.js
    Co-Authored-By: RockAmit <115550644+RockAmit@users.noreply.github.com>

---
+++ RemoveMaximumElement.js
@@ -0,0 +1,83 @@
+// Remove Maximum Element In Linked List Ended
+// Description
+
+// You are given the head of the linked list, you need to remove the maximum element in the linked list and return the head of the changed linked list.
+
+// Note
+
+// You don’t need to take input/output. Just implement the given function.
+
+// If the linked list is empty, return a NULL value
+
+// If there is more than one maximum element remove the last occurring element in the linked list.
+
+
+// Input
+// The first line contains the size of the linked list.
+
+// The second line contains the linked list in form of an array.
+
+// Function description
+
+
+// removeMaximum(head) {
+
+// // Write your code here
+
+// }
+
+// Arguments:
+
+// head = Head node of the linked list.
+// Constraints
+
+// 1 <= N <= 10^4
+
+// 1 <= Linked List Values <= 500
+
+
+// Output
+// Return the head of the modified linked list.
+
+
+// Sample Input 1 
+
+// 5
+// 1 2 3 4 5
+// Sample Output 1
+
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+// -1
+
+const LinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+// Complete the function below
+var removeMaximum = function (head) {
+   let smallest = head; 
+   let temp = head;
+   let prev = null;
+
+   while(temp != null) {
+      if(temp.next != null && temp.next.data >= smallest.data){
+        smallest = temp.next;
+        prev = temp;
+      }
+     temp = temp.next;
+   }
+
+   if(smallest != head) { //First element is not min
+      prev.next = smallest.next;
+   } else {
+       head = head.next; //If first element is smallest, update the head
+   }
+   
+   return head;
+};
