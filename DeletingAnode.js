eb270b1ae6280bc50b76a50b0ae8d92d9e93fb6b
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2022-12-11 17:34:02 +0530

    Create DeletingAnode.js
    Co-Authored-By: RockAmit <115550644+RockAmit@users.noreply.github.com>

---
+++ DeletingAnode.js
@@ -0,0 +1,70 @@
+// Deleting a Node Ended
+// Description
+
+// Delete the node at a given position in a linked list and return a reference to the head node. The head is at position 0. The list may be empty after you delete the node. In that case, return a null value.
+
+// Complete thedeleteNodefunction in the editor below.
+
+// deleteNodehas the following parameters:
+// -LinkedListNode pointer list:a reference to the head node in the list
+// -int position:the position of the node to remove
+
+
+// Input
+// The first line of input contains an integer n, the number of elements in the linked list.
+
+// Each of the next n linescontains an integer, the node data values in order.
+
+// The last line contains an integer,the position of the node to delete.
+
+// n <= 1000
+
+// list[i] <= 1000
+
+
+// Output
+// Print the updated Linked List
+
+
+// Sample Input 1 
+
+// 8
+// 20
+// 6
+// 2
+// 19
+// 7
+// 4
+// 15
+// 9
+// 3
+// Sample Output 1
+
+// 20 6 2 7 4 15 9
+
+const LinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+// Complete the function below
+
+function deleteNode(head, position) {
+     
+    if(position==0){
+        return head.next;
+         return head;
+    }
+ 
+   let count=0;
+   let current =head;
+   while(count!=position-1){
+       current = current.next;
+       count++;
+   }
+   current.next = current.next.next;
+   return head;
+  
+}
