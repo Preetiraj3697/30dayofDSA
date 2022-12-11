6bf783b4bfb931fdd725e531a726206f0a0ac71e
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2022-12-11 17:20:35 +0530

    Create ListAndQueue2.js
    Co-Authored-By: RockAmit <115550644+RockAmit@users.noreply.github.com>

---
+++ ListAndQueue2.js
@@ -0,0 +1,38 @@
+// List and Queue 2 Ended
+// Description
+
+// You need to implement a list using queue data structure. You can only use queue.get() and queue.put() methods.
+
+// Note- import and the use queue data structure.
+
+
+// Input
+// The first line contains the number of operations (t).
+
+// Next t lines contains commands for the list.
+
+// Eg- Push 3 means add 3 in the list
+
+// Pop means to remove the last element of the list and print it on the screen
+
+// Note-If the list is empty print- 'Empty' (Without the quotes)
+
+// t <= 500000
+
+
+// Output
+// Print the last element as a single integer on the screen.
+
+
+// Sample Input 1 
+
+// 3
+// Push 4
+// Push 5
+// Pop
+// Sample Output 1
+
+// 5
+// Hint
+
+// After two push operations the list would look like - [4,5], popping it should return 5
